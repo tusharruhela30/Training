e6ebbfa29bc75b0f9cb97578bbf903842eac2a2d
divyanshuelred <divyanshu@elred.io>
2023-09-20 16:02:04 +0530

    Section2 complete

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,84 @@
 let currentName = "Divyanshu";
+let log = [];
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-alert(`Hi i am ${currentName}, Section 2 is in WIP Done by tomorrow`);
+ 
+// Generate result
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+// get input values
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function writeToLog(operationIdentifier,prevResult,operationNumber,newResult) {
+    const logEntry = { operation: operationIdentifier,
+      prevResult: prevResult,
+      number: operationNumber,
+      result: newResult
+    };
+    
+    log.push(logEntry);
+    console.log(log);
+  }
+
+// Adding
+function add() {
+  // currentResult = currentResult + parseInt(userInput.value); Parse way
+  //   currentResult = currentResult + +userInput.value; // other way
+  //   outputResult(currentResult, '');
+
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+// Subtracting
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  console.log("1");
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+}
+
+ 
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+
+// Section 2 Assignment
+// function subtract(n1,n2){
+//     return n1-n2;
+//     alert("1"); // It wont work
+// }
+function stringify(n1) {
+  return `Result: ${n1}`;
+}
+
+subtract(12, 10);
+stringify(10);
